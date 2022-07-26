b4e03fe1622710df6679e9a2724d33b94fa5c82d
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-07-26 18:12:51 -0300

    Conclui o nono exercício

--- src/challenges.js
+++ src/challenges.js
@@ -85,11 +85,16 @@ function fizzBuzz(array) {
 
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  string = string.replace(/a/g, 1).replace(/e/g, 2).replace(/i/g, 3).replace(/o/g, 4).replace(/u/g, 5)
+  return string
+   
 }
-function decode() {
-  // seu código aqui
+
+
+function decode(string) {
+  string = string.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u')
+  return string
 }
 
 // Desafio 10
