db6fb570b28c24f7acfe8018f205846987290bf6
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-07-27 12:59:55 -0300

    finaliza o exercicio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,35 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let triangleCheck = 0;
+  if ((lineA > (lineB + lineC) || lineA < Math.abs(lineB-lineC))  || (lineB > (lineA+lineC) || lineB < Math.abs(lineA-lineC)) || (lineC > (lineA+lineB) || lineC < Math.abs(lineB-lineA))){
+    return false
+  }
+   return true
 }
+
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
+  // String recebida:
+  '1 cerveja'
+
+// String retornada:
+  '1 copo de água'
+// String recebida:
+  '1 cachaça, 5 cervejas e 1 copo de vinho'
+
+// String retornada:
+  '7 copos de água'
+// String recebida:
+  '1 cachaça, 5 cervejas e 1 copo de vinho'
+
+// String retornada:
+  '7 copos de água'
 }
 
 module.exports = {
