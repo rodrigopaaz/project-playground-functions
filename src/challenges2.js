06408576eab8a02598116c24d8c82c9be168df24
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-07-27 16:20:30 -0300

    Finaliza o exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,43 @@
 // Desafio 11
 function generatePhoneNumber(array) {
+  let prefixo = ''
+  let num1 = ''
+  let num2 = ''
 
-}
+  
+   for(let index2 in array){
+  let check = 0
+  let check2 = 0
+    check = array[index2]
+    for (let index3 in array){
+    if (check === array[index3]){
+      check2 += 1}
+    }
+    if (check2>=3){
+        return 'não é possível gerar um número de telefone com esses valores'
+      }
+    } 
+      
+      if(array.length !== 11){
+        return "Array com tamanho incorreto."
+  }   
+  
+    for (let index in array){
+      if (array[index]<0 || array[index]>9){return 'não é possível gerar um número de telefone com esses valores'}
+    }
+
+  
+    array = array.join('')
+    prefixo = array.substring(0,2)
+    num1 = array.substring(2,7)
+    num2 = array.substring(7,11)
+     return '('+prefixo+')'+' '+num1+'-'+num2
+   
+  }
+    
+
+  
+
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -14,22 +50,16 @@ function triangleCheck(lineA, lineB, lineC) {
 
 
 // Desafio 13
-function hydrate() {
-  // String recebida:
-  '1 cerveja'
+function hydrate(string) {
+  const replaced = string.replace(/\D/g, ''); 
+  number=replaced
+  number = number.split();
+  let soma = 0;
+  for (index in number){
+    soma += number[index]
+  }
+return soma + ' copo de água'
 
-// String retornada:
-  '1 copo de água'
-// String recebida:
-  '1 cachaça, 5 cervejas e 1 copo de vinho'
-
-// String retornada:
-  '7 copos de água'
-// String recebida:
-  '1 cachaça, 5 cervejas e 1 copo de vinho'
-
-// String retornada:
-  '7 copos de água'
 }
 
 module.exports = {
